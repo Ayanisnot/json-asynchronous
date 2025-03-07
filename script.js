e5280c16df88c3ab41a458a3cd8170443f1359e9
Ayanisnot <alayanhussain0@gmail.com>
2025-03-07 15:56:04 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-async function product(a, b) {
-    let response = a * b;
-    display(response); 
+var choice = prompt("welcome to AREA CALCULTOR \n ENTER YOUR CHOICE \n1.Area of rectangle \n2.Area of triangle \n3.Area of circle \n4.Area of paralellogram");
+if (choice == '1' ) {
+
+    var l = prompt ('Enter the length');
+    var b = prompt ('Enter the breadth');
+    var result = Number(l) *  Number(b);
+    alert('the area of rectangle is' + result);
 }
- function display(some) {
-document.getElementById("result").innerHTML = some;
- }
- product(5, 20);
+
+else if (choice == '2') {
+   var h = prompt ('enter the height');
+   var b = prompt ('ente the base');
+   var result = number(h) * number(b);
+   alert()
+
+  
+
+}
+    
